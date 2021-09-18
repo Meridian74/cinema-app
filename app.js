a3ff7061fa302089d84c0af1a199ab7c8a42bc83
Meridian74 <meridian@freemail.hu>
2021-09-18 23:27:22 +0200

    Implemented basic logic

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const amount = document.getElementById("amount");
 let ticketPrice = 0;
 let allSeats = 0;
 
+function updateCounterAndPrice() {
+   const selectedSeats = document.querySelectorAll(".seats .available-seat.selected");
+   counter.innerText = selectedSeats.length;
+   amount.innerText = selectedSeats.length * ticketPrice;
+}
+
 const populateMovieList = () => {
    movieList.movies.forEach(movie => {
       const option = document.createElement("option");
@@ -35,15 +41,40 @@ const populateSeats = () => {
 };
 
 const populateFromLOcalStorage = () => {
-   
+
 };
 
 const populatedUI = () => {
    populateMovieList();
    populateSeats();
-
    populateFromLOcalStorage();
 };
 
 
 populatedUI();
+
+movieSelected.addEventListener("change", e => {
+   ticketPrice = e.target.value;
+   updateCounterAndPrice()
+});
+
+seatsContainer.addEventListener("click", e => {
+   if (
+      e.target.classList.contains("available-seat") &&
+      !e.target.classList.contains("occupied")
+   ) {
+      e.target.classList.toggle("selected");
+      updateCounterAndPrice()
+   }
+});
+
+resetButton.addEventListener("click", () => {
+   document
+      .querySelectorAll(".seats .available-seat.selected")
+      .forEach(seat => seat.classList.remove("selected"));
+   
+   counter.innerText = 0;
+   amount.innerText = 0;
+   movieSelected.selectedIndex = 0;
+   ticketPrice = movieSelected.value;
+});
